8ad6be1ae645f285956b4734185650730c83726f
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-15 23:42:51 +0900

    PGS_문자열 출력하기

---
+++ woojae/0715/PGS_문자열_출력하기.java
@@ -0,0 +1,10 @@
+package 프로그래머스;
+import java.util.Scanner;
+
+public class PGS_문자열_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
