e44d5571f873116cd2868de97b46bff91c7d0e4c
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-15 23:30:50 +0900

    PGS_첫 번째로 나오는 음수

--- woojae/0715/PGS_첫 번째로 나오는 음수.java
+++ woojae/0715/PGS_첫 번째로 나오는 음수.java
@@ -1,4 +1,12 @@
-class Solution2 {
+public class PGS_첫_번째로_나오는_음수 {
+    public static void main(String[] args) {
+        int[] num_list = {12, 4, 15, 46, 38, -2, 15};
+        PGS_첫_번째로_나오는_음수_Solution solution = new PGS_첫_번째로_나오는_음수_Solution();
+        System.out.println(solution.solution(num_list));
+    }
+}
+
+class PGS_첫_번째로_나오는_음수_Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         for(int num: num_list) {
