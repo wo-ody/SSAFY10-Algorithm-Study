3a775e88331c414b4914a86d7a52b7e3a1d33a3f
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-15 23:30:29 +0900

    PGS_공배수

--- woojae/0715/PGS_공배수.java
+++ woojae/0715/PGS_공배수.java
@@ -1,4 +1,14 @@
-class Solution1 {
+public class PGS_공배수 {
+    public static void main(String[] args) {
+        int number = 60;
+        int n = 2;
+        int m = 3;
+        PGS_공배수_Solution solution = new PGS_공배수_Solution();
+        System.out.println(solution.solution(number, n, m));
+    }
+}
+
+class PGS_공배수_Solution {
     public int solution(int number, int n, int m) {
         int answer = 0;
         answer = (number % n == 0 && number % m == 0) ? 1 : 0;
