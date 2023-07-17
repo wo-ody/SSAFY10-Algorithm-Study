1257a8601b3f7ea9b88212f79848b518a97ae7d9
luke0421 <73464649+luke0421@users.noreply.github.com>
2023-07-17 22:47:15 +0900

    BOJ_11718_그대로출력하기.java

---
+++ hyeonguk/20230717/BOJ_11718_그대로출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+
+	}
+
+}
