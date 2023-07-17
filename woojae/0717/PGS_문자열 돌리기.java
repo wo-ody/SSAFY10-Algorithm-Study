c350b4f3283c91bd9c9d67e63cc6aec7d19110c8
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-17 10:45:36 +0900

    PGS_문자열 돌리기

---
+++ woojae/0717/PGS_문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] str = a.toCharArray();
+        for (char s : str)
+        {
+            System.out.println(s);
+        }
+    }
+}
