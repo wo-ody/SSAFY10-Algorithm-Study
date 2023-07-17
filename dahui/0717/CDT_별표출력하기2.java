8918d620178fd4c8f4db558702c3042edd6165dd
dahui0525 <74452898+dahui0525@users.noreply.github.com>
2023-07-17 21:20:25 +0900

    CDT_별표출력하기2

---
+++ dahui/0717/CDT_별표출력하기2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
