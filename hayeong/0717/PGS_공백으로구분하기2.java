c5c279b838b1290232ce71b377434358c6cdf2f6
rlagkdud <43032875+rlagkdud@users.noreply.github.com>
2023-07-17 22:18:46 +0900

    PGS_공백으로구분하기2.java

--- hayeong/0717/PGS_공백으로구분하기2.java
+++ hayeong/0717/PGS_공백으로구분하기2.java
@@ -1,19 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class 공백으로구분하기2 {
     public String[] solution(String my_string) {
-        String[] answer = my_string.split(" ");
-        List<String> list = new ArrayList<>();
-        for(int i = 0; i<answer.length; i++){
-            if(!answer[i].equals("")){
-                list.add(answer[i]);
-            }
-        }
-        String[] result = new String[list.size()];
-        for(int i= 0; i< result.length; i++){
-            result[i] = list.get(i);
-        }
-        return result;
+        return Arrays.stream(my_string.trim().split("")).filter(x->!x.equals("")).toArray(String[]::new);
     }
 }
