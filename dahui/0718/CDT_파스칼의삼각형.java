a4c2f37237f8a266448c47dc8807bdb5d2508f6b
dahui0525 <74452898+dahui0525@users.noreply.github.com>
2023-07-18 23:57:03 +0900

    Create CDT_파스칼의삼각형.java

---
+++ dahui/0718/CDT_파스칼의삼각형.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                arr[i] = new int[j+1];
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                if(j==0){
+                    arr[i][j] = 1;
+                }
+                else if(i==j){
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+                }
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
