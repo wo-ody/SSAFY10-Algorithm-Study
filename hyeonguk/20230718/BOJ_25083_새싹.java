170b7e8a812e30dfffb9a89319703b041e39dfc7
luke0421 <73464649+luke0421@users.noreply.github.com>
2023-07-18 20:57:21 +0900

    BOJ_25083_새싹.java

---
+++ hyeonguk/20230718/BOJ_25083_새싹.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
