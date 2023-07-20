4dc6007e125f6069fcaff12c1a72970c82a1a7d9
dahui0525 <74452898+dahui0525@users.noreply.github.com>
2023-07-20 16:08:35 +0900

    CDT_파스칼의삼각형

--- dahui/0718/CDT_파스칼의삼각형.java
+++ dahui/0718/CDT_파스칼의삼각형.java
@@ -26,7 +26,6 @@ public class Main {
                 }
             }
         }
-
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
                 System.out.print(arr[i][j] + " ");
