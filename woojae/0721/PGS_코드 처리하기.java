83bb14d12a90631b796d7784745ac36a4a12623a
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-21 09:42:57 +0900

    PGS_코드 처리하기

--- woojae/0721/PGS_코드 처리하기.java
+++ woojae/0721/PGS_코드 처리하기.java
@@ -5,29 +5,13 @@ class Solution {
         int len = arr.length;
         int mode = 0;
         for (int idx = 0; idx < len; idx++) {
-            if (mode == 0) {
-                if (arr[idx] == '1') {
-                    mode = 1;
-                    continue;
-                }
-                else {
-                    if (idx % 2 == 0) {
-                        ret += arr[idx];
-                    }
-                }
+            if (arr[idx] == '1') {  // 현재 code 값이 1일때
+                mode = (mode == 0) ? 1 : 0;  // 현재 mode 값에 따라 mode 값 갱신
+                continue;
             }
-            else {
-                if (arr[idx] == '1') {
-                    mode = 0;
-                    continue;
-                }
-                else {
-                    if (idx % 2 != 0) {
-                        ret += arr[idx];
-                    }
-                }
-            }
+            if (idx % 2 == mode)  // 현재 code 값이 1이 아닐때 idx가 mode와 같다면(짝수일 땐 나머지가 0, 홀수일 땐 1)
+                ret += arr[idx];  // ret의 마지막에 해당 값 추가
         }
-        return ret.equals("") ? "EMPTY" : ret;
+        return ret.equals("") ? "EMPTY" : ret;  // 빈 문자열 여부 확인 후, 리턴 값 선택
     }
 }
