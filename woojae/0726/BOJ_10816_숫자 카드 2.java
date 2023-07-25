eac0b48fdba591cb4ca23e7330f6366c1cf808d4
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-07-26 01:52:54 +0900

    BOJ_10816_숫자 카드 2.java

--- woojae/0726/BOJ_10816_숫자 카드 2.java
+++ woojae/0726/BOJ_10816_숫자 카드 2.java
@@ -10,22 +10,22 @@ public class Main {
 		int k = 0;
 		
 		for(int i = 0; i < n; i++) {
-			k = sc.nextInt();
-			if (cards.containsKey(k))
-				cards.put(k, cards.get(k) + 1);
+			k = sc.nextInt();  // 데이터 입력
+			if (cards.containsKey(k))  // 입력된 데이터가 있다면
+				cards.put(k, cards.get(k) + 1);  // 해당 데이터의 수 갱신
 			else
-				cards.put(k, 1);
+				cards.put(k, 1);  // 없다면 1로 초기화
 		}
 		
 		int m = sc.nextInt();
 		for(int i = 0; i < m; i++) {
-			k = sc.nextInt();
-			if (cards.containsKey(k))
-				sb.append(cards.get(k)).append(' ');
+			k = sc.nextInt();  // 데이터 입력
+			if (cards.containsKey(k))  // 입력된 데이터가 있다면
+				sb.append(cards.get(k)).append(' ');  // 출력을 위해 저장
 			else
-				sb.append(0).append(' ');
+				sb.append(0).append(' ');  // 없다면 0 저장
 		}
 			
-		System.out.println(sb.toString().substring(0, sb.length() - 1));
+		System.out.println(sb.toString().substring(0, sb.length() - 1));  // 출력 형태에 맞춰 포매팅
 	}
 }
