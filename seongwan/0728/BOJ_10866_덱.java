22fbefa300819ebd6cb801f2a805710021c830a3
seongwan lee <cjddktkdwk@naver.com>
2023-07-29 01:29:28 +0900

    BOJ_10866_덱.java

--- seongwan/0728/BOJ_10866_덱.java
+++ seongwan/0728/BOJ_10866_덱.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) throws NoSuchElementException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Deque<Integer> queue = new LinkedList<>(); // 연결리스트로 큐 구현
+		Deque<Integer> deque = new LinkedList<>(); // 연결리스트로 덱 구현
 
 		int N = Integer.parseInt(br.readLine());
 
@@ -18,23 +18,23 @@ public class Main {
 			String[] str = s.split(" ");
 			if (str[0].equals("push_back")) {
 
-				queue.addLast((Integer.parseInt(str[1])));// push문자가 나올 시 다음에 나올 정수 add
+				deque.addLast((Integer.parseInt(str[1])));// push_back 문자가 나올 시 덱 마지막에 다음에 나올 정수 add
 
 			} else if (str[0].equals("push_front")) {
-				queue.addFirst(Integer.parseInt(str[1]));// push문자가 나올 시 다음에 나올 정수 add
+				deque.addFirst(Integer.parseInt(str[1]));// push_front 문자가 나올 시 처음에 다음에 나올 정수 add
 
 			} else if (s.equals("front")) {
-				System.out.println(queue.size() == 0 ? -1 : queue.peekFirst());
+				System.out.println(deque.size() == 0 ? -1 : deque.peekFirst());
 			} else if (s.equals("back")) {
-				System.out.println(queue.size() == 0 ? -1 : queue.peekLast());
+				System.out.println(deque.size() == 0 ? -1 : deque.peekLast());
 			} else if (s.equals("size")) {
-				System.out.println(queue.size());
+				System.out.println(deque.size());
 			} else if (s.equals("empty")) {
-				System.out.println(queue.isEmpty() ? 1 : 0);
+				System.out.println(deque.isEmpty() ? 1 : 0);
 			} else if (s.equals("pop_back")) {
-				System.out.println(queue.size() == 0 ? -1 : queue.pollLast());
+				System.out.println(deque.size() == 0 ? -1 : deque.pollLast());
 			} else if (s.equals("pop_front")) {
-				System.out.println(queue.size() == 0 ? -1 : queue.pollFirst());
+				System.out.println(deque.size() == 0 ? -1 : deque.pollFirst());
 			}
 
 		}
