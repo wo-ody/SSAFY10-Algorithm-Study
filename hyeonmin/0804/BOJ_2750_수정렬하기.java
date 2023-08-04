b60c53e5896dd66f8df051641680cc95fcbccb20
HYEONMIN LEE <hyunmin2667@gmail.com>
2023-08-05 02:03:01 +0900

    BOJ_2750_수정렬하기

---
+++ hyeonmin/0804/BOJ_2750_수정렬하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_2750_수정렬하기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(A);
+		for (int i = 0; i < N; i++) {
+			System.out.println(A[i]);
+		}
+	}
+}
