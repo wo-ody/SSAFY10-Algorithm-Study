0fc18e0029e2b6085752f74d9b3634e6cfaf6f40
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2023-08-07 16:31:12 +0900

    BOJ_7568_덩치

--- woojae/0807/BOJ_7568_덩치.java
+++ woojae/0807/BOJ_7568_덩치.java
@@ -25,15 +25,15 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			weight = Integer.parseInt(st.nextToken());
 			height = Integer.parseInt(st.nextToken());
-			arr[i] = new Info(weight, height);
+			arr[i] = new Info(weight, height);  // 몸무게와 키를 입력 받은 객체를 생성하여 저장
 		}
 		
 		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++) 
-				if(i == j || (arr[j].weight > arr[i].weight && arr[j].height > arr[i].height))
+			for (int j = 0; j < n; j++) // 개개인의 기본 순위는 1, 하지만 배열은 처음에 0으로 초기화 되어있으므로 자기 자신을 비교할 때 +1 갱신
+				if(i == j || (arr[j].weight > arr[i].weight && arr[j].height > arr[i].height))  // 이후 자기보다 크다면
 					answer[i] += 1;
 		
-		for (int i : answer) sb.append(i + " ");
+		for (int i : answer) sb.append(i + " ");  // 순위가 갱신된 정보를 하나의 문자열로 저장
 		System.out.println(sb);
 	}
 }
