3f402d13a61223ab97cd512384afd96aa829b630
dahui0525 <74452898+dahui0525@users.noreply.github.com>
2023-08-21 21:39:59 +0900

    BOJ_1388_바닥장식

---
+++ dahui/0820/BOJ_1388_바닥장식.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M;
+	static char[][] arr;
+	static boolean[][] check;
+	static int cnt;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new char[N][M];
+		check = new boolean[N][M];
+		
+		for(int i=0; i<N; i++) {
+			String str = br.readLine();
+			for(int j=0; j<M; j++) {
+				arr[i][j] = str.charAt(j);
+			}
+		}
+		
+		simulation();
+		
+		System.out.println(cnt);
+	}
+		
+		static void simulation() {
+			Deque<Character> stack = new ArrayDeque<>();
+			
+			for(int i=0;i<N;i++) {
+				for(int j=0; j<M; j++) {
+					if(arr[i][j] == '-') {
+						stack.add('-');
+					}else {
+						if(!stack.isEmpty()) {
+							stack.clear();
+							cnt++;
+						}
+					}
+				}
+				if(!stack.isEmpty()) {
+					stack.clear();
+					cnt++;
+				}
+				
+			}
+			
+			for(int i=0;i<M;i++) {
+				for(int j=0; j<N; j++) {
+					if(arr[j][i] == '|') {
+						stack.add('|');
+					}else {
+						if(!stack.isEmpty()) {
+							stack.clear();
+							cnt++;
+						}
+					}
+				}
+				if(!stack.isEmpty()) {
+					stack.clear();
+					cnt++;
+				}
+			}
+		}
+}
