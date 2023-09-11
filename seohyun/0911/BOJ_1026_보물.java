9dda6ccba5beaa072e110047093921f448658222
wo-ody <45092652+wo-ody@users.noreply.github.com>
2023-09-11 23:51:51 +0900

    BOJ_1026_보물

---
+++ seohyun/0911/BOJ_1026_보물.java
@@ -0,0 +1,45 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class boj_1026_보물 {
+    static int N;
+    static int[] A;
+    static int[] B;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(simulation());
+    }
+
+    public static int simulation(){
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += (A[i] * B[N-i-1]);
+        }
+
+        return sum;
+    }
+}
