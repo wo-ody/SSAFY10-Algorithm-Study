95c1b09cc93fc36fbecdcf750215319ae6fde659
dahui0525 <74452898+dahui0525@users.noreply.github.com>
2023-09-12 00:12:23 +0900

    BOJ_1026_보물

---
+++ dahui/0911/BOJ_1026_보물.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[] A;
+	static int[] B;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		A = new int[N];
+		B = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int ans = 0;
+		
+		for(int i=0; i<N; i++) {
+			ans += A[i]*B[N-i-1];
+		}
+		
+		System.out.println(ans);
+	}
+
+}
