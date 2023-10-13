d7e77c0810c22f5e756c9637dc4dfe4306776ee8
JongKook <94429120+JongKookE@users.noreply.github.com>
2023-10-13 10:31:04 +0900

    BOJ_11723_집합

--- jongkook/10-12/BOJ_11723_집합.java
+++ jongkook/10-12/BOJ_11723_집합.java
@@ -1,5 +1,5 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.BufferedWriter; 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
