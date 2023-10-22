97b350887e6b9b636b4c3d74e0602f04ffa8719c
changhi97 <77597957+changhi97@users.noreply.github.com>
2023-10-23 00:17:31 +0900

    BOJ_15724_주지수

---
+++ changhee/1023/BOJ_15724_주지수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; ++i) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; ++j) {
+                map[i][j] = Integer.parseInt(st.nextToken()) + map[i - 1][j] + map[i][j - 1] - map[i - 1][j - 1];
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        StringBuilder answer =new StringBuilder();
+        for (int i = 0; i < K; ++i) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int res = map[x2][y2] - map[x2][y1 - 1] - map[x1 - 1][y2] + map[x1 - 1][y1 - 1];
+
+            answer.append(res).append("\n");
+        }
+        System.out.println(answer);
+    }
+}
