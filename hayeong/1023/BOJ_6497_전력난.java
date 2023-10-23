8255115d9812aa9a4aa6ab61b7062eeede487515
rlagkdud <43032875+rlagkdud@users.noreply.github.com>
2023-10-23 23:46:12 +0900

    BOJ_6497_전력난

--- hayeong/1023/BOJ_6497_전력난.java
+++ hayeong/1023/BOJ_6497_전력난.java
@@ -10,67 +10,73 @@ public class BOJ_6497_전력난 {
 
     static int[] parents;
 
-    static void make(){
-        parents = new int[M+1];
-        for(int i =1; i<=M; i++){
+    static void make() {
+        parents = new int[M + 1];
+        for (int i = 1; i <= M; i++) {
             parents[i] = i;
         }
     }
 
-    static int find(int a){
-        if(parents[a] == a) return a;
+    static int find(int a) {
+        if (parents[a] == a) return a;
         return parents[a] = find(parents[a]);
     }
 
-    static boolean union(int a, int b){
+    static boolean union(int a, int b) {
         int rootA = find(a);
         int rootB = find(b);
-        if(rootA == rootB) return false;
+        if (rootA == rootB) return false;
         parents[rootB] = rootA;
         return true;
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-
-        int sum = 0;
-        edgeList = new Edge[N];
-        for(int i = 0; i<N; i++){
+        StringTokenizer st;
+        while (true) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            sum += cost;
-            edgeList[i] = new Edge(from, to, cost);
-        }
-        br.readLine();
 
-        Arrays.sort(edgeList);
-        make();
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            if (M == 0 && N == 0) break;
 
-        int result = 0;
-        int cnt = 0;
-        for(Edge e : edgeList){
-            if(union(e.from, e.to)){
-                result+=e.cost;
-                if(++cnt == M-1) break;
+            int sum = 0;
+            edgeList = new Edge[N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                int cost = Integer.parseInt(st.nextToken());
+                sum += cost;
+                edgeList[i] = new Edge(from, to, cost);
             }
+
+            Arrays.sort(edgeList);
+            make();
+
+            int result = 0;
+            int cnt = 0;
+            for (Edge e : edgeList) {
+                if (union(e.from, e.to)) {
+                    result += e.cost;
+                    if (++cnt == M - 1) break;
+                }
+            }
+            System.out.println(sum - result);
         }
-        System.out.println(sum -result);
 
     }
 
-    static class Edge implements Comparable<Edge>{
+    static class Edge implements Comparable<Edge> {
         int from, to, cost;
-        public Edge(int from, int to, int cost){
+
+        public Edge(int from, int to, int cost) {
             this.from = from;
             this.to = to;
             this.cost = cost;
         }
 
-        public int compareTo(Edge o){
+        public int compareTo(Edge o) {
             return this.cost - o.cost;
         }
     }
