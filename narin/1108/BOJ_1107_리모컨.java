b028371bbd7c3e7e818d3cf90e0c3dac73373026
nana <139676399+xw2nana@users.noreply.github.com>
2023-11-08 22:53:22 +0900

    Update BOJ_1107_리모컨.java

--- narin/1108/BOJ_1107_리모컨.java
+++ narin/1108/BOJ_1107_리모컨.java
@@ -1,41 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    private static int N, M, result;
-    private static boolean[] broken;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        broken = new boolean[10];
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        boolean[] broken = new boolean[10];
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int temp = Integer.parseInt(st.nextToken());
-            broken[temp] = true;
+        if(M!=0) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                int temp = Integer.parseInt(st.nextToken());
+                broken[temp] = true;
+            }
         }
 
-        result  = Math.abs(N-100);
+        int result = Math.abs(N - 100);
 
-        for(int i =0 ; i<1000000; i++){
+        for (int i = 0; i <= 999999; i++) {
             String num = String.valueOf(i);
 
             boolean isBreak = false;
-            for(int j =0; j<num.length(); j++){
-                if(broken[num.charAt(j)-'0']){
+            for (int j = 0; j < num.length(); j++) {
+                if (broken[num.charAt(j) - '0']) {
                     isBreak = true;
                     break;
                 }
             }
 
-            if(!isBreak){
-                int min = Math.abs(N - i)+num.length();
+            if (!isBreak) {
+                int min = Math.abs(N - i) + num.length();
                 result = Math.min(min, result);
             }
         }
