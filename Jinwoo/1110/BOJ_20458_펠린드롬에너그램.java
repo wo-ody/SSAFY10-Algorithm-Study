45d978c3db1cf91ca6c1b569b2c828d9fa4569b5
cJinu <chjnw0204@gmail.com>
2023-11-10 09:37:31 +0900

    Create BOJ_20458_펠린드롬에너그램.java

---
+++ Jinwoo/1110/BOJ_20458_펠린드롬에너그램.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		char[] palin = br.readLine().toCharArray();
+		int[] alphCnt = new int[26];
+		for (int i=0; i<n/2; i++) {
+			alphCnt[palin[i]-'a']++;
+		}
+		for (int i=n/2+((n%2==0)? 0:1); i<n; i++) {
+			alphCnt[palin[i]-'a']++;
+		}
+		boolean flag = true;
+		for (int i:alphCnt) {
+			if (i%2==1) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) System.out.println("Yes");
+		else System.out.println("No");
+	}
+
+}
