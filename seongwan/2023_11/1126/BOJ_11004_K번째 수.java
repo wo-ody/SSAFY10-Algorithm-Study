6497f3f3e8b798389d046962cb504196eac65bc0
seong-wan <139252411+seong-wan@users.noreply.github.com>
2023-11-27 03:53:14 +0900

    BOJ_11004_K번째 수.java

---
+++ seongwan/2023_11/1126/BOJ_11004_K번째 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N, K;
+	static int[] input;
+
+	public static void main(String[] args) throws Exception {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		input = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(input);
+
+		System.out.println(input[K - 1]);
+
+	}
+}
