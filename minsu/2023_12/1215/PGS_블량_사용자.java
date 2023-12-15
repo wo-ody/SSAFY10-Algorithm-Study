407353d8332e10e506168a528222998bb3a0023e
Minsu Kang <alstn7223@gmail.com>
2023-12-15 14:20:51 +0900

    Create PGS_블량_사용자.java

---
+++ minsu/2023_12/1215/PGS_블량_사용자.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    String[] userIds;
+    String[] bannedIds;
+    boolean[] visited;
+    HashSet<HashSet<String>> result = new HashSet<>();
+
+    public int solution(String[] user_id, String[] banned_id) {
+        userIds = user_id;
+        bannedIds = banned_id;
+        visited = new boolean[userIds.length];
+
+        dfs(new HashSet<>(), 0);
+
+        return result.size();
+    }
+
+    void dfs(HashSet<String> set, int depth) {
+        if (depth == bannedIds.length) {
+            result.add(set);
+            return;
+        }
+
+        for (int i = 0; i < userIds.length; i++) {
+            if (set.contains(userIds[i])) {
+                continue;
+            }
+
+            if (check(userIds[i], bannedIds[depth])) {
+                set.add(userIds[i]);
+                dfs(new HashSet<>(set), depth + 1);
+                set.remove(userIds[i]);
+            }
+        }
+    }
+
+    boolean check(String userId, String bannedId) {
+        if (userId.length() != bannedId.length()) {
+            return false;
+        }
+
+        boolean match = true;
+        for (int i = 0; i < userId.length(); i++) {
+            if (bannedId.charAt(i) != '*' && userId.charAt(i) != bannedId.charAt(i)) {
+                match = false;
+                break;
+            }
+        }
+
+        return match;
+    }
+}
