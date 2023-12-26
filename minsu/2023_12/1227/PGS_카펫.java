009b39947b9055917ef0ded29740cce174be7010
Minsu Kang <alstn7223@gmail.com>
2023-12-26 22:12:54 +0900

    Create PGS_카펫.java

---
+++ minsu/2023_12/1227/PGS_카펫.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        
+        for (int i = 3; i < sum; i++) {
+            int j = sum / i;
+            
+            if (sum % i == 0 && j >= 3) {
+                int col = Math.max(i, j);
+                int row = Math.min(i, j);
+                int center = (col - 2) * (row - 2);
+                
+                if (center == yellow) {
+                    answer[0] = col;
+                    answer[1] = row;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+}
