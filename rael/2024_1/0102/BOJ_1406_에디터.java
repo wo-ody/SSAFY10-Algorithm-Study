361390a615c314ff846d40e06383a95b0ba3cf2e
Rael <allice2283006@gmail.com>
2024-01-03 00:59:26 +0900

    BOJ_1406_에디터

---
+++ rael/2024_1/0102/BOJ_1406_에디터.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.Exception;
+import java.io.InputStreamReader;
+import java.util.Stack;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> leftStack = new Stack<>();
+        Stack<Character> rightStack = new Stack<>();
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++) {
+            leftStack.push(s.charAt(i));
+        }
+        int num = Integer.parseInt(br.readLine());
+        for (int i = 0; i < num; i++) {
+            String command = br.readLine();
+            char c = command.charAt(0);
+            if (c == 'P') {
+                leftStack.push(command.charAt(2));
+            } else if (c == 'L' && !leftStack.isEmpty()) {
+                rightStack.push(leftStack.pop());
+            } else if (c == 'D' && !rightStack.isEmpty()) {
+                leftStack.push(rightStack.pop());
+            } else if (c == 'B' && !leftStack.isEmpty()) {
+                leftStack.pop();
+            }
+ 
+        }
+        while (!leftStack.isEmpty()) {
+            rightStack.push(leftStack.pop());
+        }
+        while (!rightStack.isEmpty()) {
+            sb.append(rightStack.pop());
+        }
+        System.out.println(sb.toString());
+ 
+    }
+}
