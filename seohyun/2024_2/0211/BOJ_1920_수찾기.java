080a3ac77fba6c8ee9083cd3c6ca775277fda1c1
wo-ody <45092652+wo-ody@users.noreply.github.com>
2024-02-12 01:35:34 +0900

    BOJ_1920_수찾기

---
+++ seohyun/2024_2/0211/BOJ_1920_수찾기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] input;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		input = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input); // natural ordering - asc
+		
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(Arrays.binarySearch(input, num) >= 0) sb.append(1).append("\n");
+			else sb.append(0).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
