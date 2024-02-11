8a16d712285ac93365e7998d61fcf30e8bc77017
wo-ody <45092652+wo-ody@users.noreply.github.com>
2024-02-12 01:34:54 +0900

    BOJ_1920_달팽이

--- seohyun/2024_2/0211/BOJ_1920_달팽이.java
+++ seohyun/2024_2/0211/BOJ_1920_달팽이.java
@@ -1,34 +1,67 @@
+package boj;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
-	static int N, M;
-	static int[] input;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		input = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(input); // natural ordering - asc
-		
-		M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(Arrays.binarySearch(input, num) >= 0) sb.append(1).append("\n");
-			else sb.append(0).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+public class boj_1913_달팽이 {
+    static int N, num;
+    static int[][] map;
 
+    static int rx,ry;
+
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        num = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+
+        simulation();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(map[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        sb.append(rx).append(" ").append(ry);
+
+        System.out.println(sb);
+    }
+
+    static void simulation(){
+
+        int su = 1;
+        int cx = N/2;
+        int cy = N/2;
+
+        map[cx][cy] = su;
+
+        int cnt = 1;
+        int dir = 0;
+
+        while(true){
+
+            for (int i = 0; i < cnt; i++) {
+                cx += dx[dir];
+                cy += dy[dir];
+                map[cx][cy] = ++su;
+
+                if(su == num){
+                    rx = cx + 1;
+                    ry = cy + 1;
+                }
+                if(cx == 0 && cy == 0) return;
+            }
+
+            dir = (dir + 1) % 4;
+
+            if(dir == 0 || dir == 2) cnt++;
+        }
+    }
 }
