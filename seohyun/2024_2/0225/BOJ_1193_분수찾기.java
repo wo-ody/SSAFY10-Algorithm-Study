02665d9069f8407f53477406193b9491621ab0fc
wo-ody <45092652+wo-ody@users.noreply.github.com>
2024-02-25 23:53:27 +0900

    BOJ_1193_분수찾기.

---
+++ seohyun/2024_2/0225/BOJ_1193_분수찾기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+ 
+		int cross_count = 1, prev_count_sum = 0;
+ 
+		while (true) {
+			// 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+			if (X <= prev_count_sum + cross_count) {	
+				
+				if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면 
+					// 분자가 큰 수부터 시작
+					// 분자는 대각선상 블럭의 개수 - (X 번째 - 직전 대각선까지의 블럭 개수 - 1) 
+					// 분모는 X 번째 - 직전 대각선까지의 블럭 개수
+					System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
+					break;
+				} 
+				
+				else {	// 대각선의 개수가 짝수라면 
+					// 홀수일 때의 출력을 반대로 
+					System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+					break;
+				}
+ 
+			} else {
+				prev_count_sum += cross_count;
+				cross_count++;
+			}
+		}
+	}
+}
