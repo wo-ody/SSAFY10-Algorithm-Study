e6be80adf26d72d49f1f836646d442f4bd6088de
dahui0525 <74452898+dahui0525@users.noreply.github.com>
2024-03-13 21:44:04 +0900

    BOJ_13549_숨바꼭질3

---
+++ dahui/2024_03/0313/BOJ_13549_숨바꼭질3.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_13549_숨바꼭질 {
+    static int N,K,ans;
+    static boolean[] visit = new boolean[2000001];
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.add(new int[] {N, 0});
+        visit[N] = true;
+
+        while (!pq.isEmpty()) {
+            int[] arr = pq.poll();
+            visit[arr[0]] = true;
+            if (arr[0] == K) {
+                ans = arr[1];
+                break;
+            }
+
+            if (arr[0] < K) {
+                if (arr[0]*2 <= 200000 && !visit[arr[0]*2]) {
+                    pq.add(new int[]{arr[0] * 2, arr[1]});
+                }
+                if (arr[0]+1 <= 200000 && !visit[arr[0]+1]) {
+                    pq.add(new int[]{arr[0] + 1, arr[1] + 1});
+                }
+            }
+            if (arr[0]-1 >= 0 && !visit[arr[0]-1]) {
+                pq.add(new int[]{arr[0] - 1, arr[1] + 1});
+            }
+
+
+        }
+        System.out.println(ans);
+    }
+}
+
