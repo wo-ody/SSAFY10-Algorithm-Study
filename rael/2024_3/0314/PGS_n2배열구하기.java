72b20a7dec51185e5fb757f94c96e7c517c6488c
Rael <allice2283006@gmail.com>
2024-03-18 01:37:14 +0900

    PGS_n2배열구하기

---
+++ rael/2024_3/0314/PGS_n2배열구하기.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public int[] solution(int n, long left, long right) {
+        int len = (int) right - (int) left;
+        int[] answer = new int[len + 1];
+
+        int idx = 0;
+        for (long i = left; i <= right; i++) {
+            long row = i / n;
+            long col = i % n;
+            answer[idx++] = Math.max((int)row, (int)col) + 1;
+        }
+        
+        return answer;
+    }
+}
