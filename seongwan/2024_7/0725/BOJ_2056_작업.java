13e531ef5256fbb27224ed40564696e2a694c33f
seongwan lee <cjddktkdwk@naver.com>
2024-07-25 22:42:00 +0900

    BOJ_2056_작업.java

---
+++ seongwan/2024_7/0725/BOJ_2056_작업.java
@@ -0,0 +1,152 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, ans;
+    static int[] pre;
+    static int[] time;
+    static List<Integer>[] postList;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> time[e1] - time[e2]);
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+
+        N = in.nextInt();
+
+        pre = new int[N];
+        time = new int[N];
+        postList = new List[N];
+        for (int i = 0; i < N; i++) {
+            postList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N; i++) {
+            time[i] = in.nextInt();
+
+            pre[i] = in.nextInt();
+            if (pre[i] == 0)
+                pq.add(i);
+
+            for (int j = 0; j < pre[i]; j++) {
+                int preNum = in.nextInt() - 1;
+                postList[preNum].add(i);
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            int temp = pq.poll();
+            ans += time[temp];
+
+            for (Integer i : pq) {
+                time[i] -= time[temp];
+            }
+
+            for (Integer i : postList[temp]) {
+                pre[i]--;
+                if (pre[i] == 0) {
+                    pq.add(i);
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) {
+                if (size < 0) return "endLine";
+            }
+            do sb.appendCodePoint(c);
+            while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32) ; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char) c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) ;
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -12345;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            } else if (c == 46) {
+                c = read();
+            }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) {
+                while (isNumber(c = read())) {
+                    n += (c - 48) / (div *= 10);
+                }
+            }
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c) {
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+}
