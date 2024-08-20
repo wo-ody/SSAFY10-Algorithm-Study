5feca5baf2accebb0458f5406ac84fe16d89a41b
seongwan lee <cjddktkdwk@naver.com>
2024-08-21 05:11:10 +0900

    BOJ_24523_내 뒤에 나와 다른 수.java

---
+++ seongwan/2024_8/0820/BOJ_24523_내 뒤에 나와 다른 수.java
@@ -0,0 +1,132 @@
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+
+        N = in.nextInt();
+        ans = new int[N + 1];
+        Arrays.fill(ans, -1);
+
+        int first = 1;
+        int last = 1;
+        int before = in.nextInt();
+
+        for (int i = 2; i <= N; i++) {
+            int after = in.nextInt();
+            //앞의 수와 다른 수가 나온 경우
+            if (before != after) {
+                for (int j = first; j <= last; j++) {
+                    ans[j] = i;
+                }
+                before = after;
+                first = i;
+                last = i;
+            }
+            //같은 수가 나온 경우
+            else {
+                last = i;
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            sb.append(ans[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    static class Reader {
+        final int SIZE = 1 << 13;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        String nextString() throws Exception {
+            StringBuilder sb = new StringBuilder();
+            byte c;
+            while ((c = read()) < 32) {
+                if (size < 0) return "endLine";
+            }
+            do sb.appendCodePoint(c);
+            while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+            return sb.toString();
+        }
+
+        char nextChar() throws Exception {
+            byte c;
+            while ((c = read()) < 32) ; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+            return (char) c;
+        }
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -1;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        long nextLong() throws Exception {
+            long n = 0;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) ;
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            }
+            do n = (n << 3) + (n << 1) + (c & 15);
+            while (isNumber(c = read()));
+            return isMinus ? ~n + 1 : n;
+        }
+
+        double nextDouble() throws Exception {
+            double n = 0, div = 1;
+            byte c;
+            boolean isMinus = false;
+            while ((c = read()) <= 32) {
+                if (size < 0) return -12345;
+            }
+            if (c == 45) {
+                c = read();
+                isMinus = true;
+            } else if (c == 46) {
+                c = read();
+            }
+            do n = (n * 10) + (c & 15);
+            while (isNumber(c = read()));
+            if (c == 46) {
+                while (isNumber(c = read())) {
+                    n += (c - 48) / (div *= 10);
+                }
+            }
+            return isMinus ? -n : n;
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        boolean isAlphabet(byte c) {
+            return (64 < c && c < 91) || (96 < c && c < 123);
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size < 0) buffer[0] = -1;
+            }
+            return buffer[index++];
+        }
+    }
+}
